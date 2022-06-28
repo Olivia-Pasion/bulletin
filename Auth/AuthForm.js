9a98fbaeb897968aba8d4d4b7ea1f26e8979167c
olivia pasion <oap9820@gmail.com>
2022-06-28 12:34:22 -0700

    auth form completed, buttons redirect properly

--- Auth/AuthForm.js
+++ Auth/AuthForm.js
@@ -1,26 +1,31 @@
+import { redirectIfLoggedIn, signInUser, signUpUser } from '../fetch-utils.js';
+
+const authSection = document.getElementById('auth-section');
+const [signIn, signUp] = authSection.querySelectorAll('form');
+const [inEmail, inPassword, upEmail, upPassword] = authSection.querySelectorAll('input');
+
+redirectIfLoggedIn();
+
+signIn.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const user = await signInUser(inEmail.value, inPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signUp.addEventListener('submit', async(e) => {
+    e.preventDefault();
+    const user = await signUpUser(upEmail.value, upPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
 
 
-export default function createAuthForm(form, { handleAuth }) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(form);
-        handleAuth(
-            formData.get('email'),
-            formData.get('password')
-        );
-    });
-    return () => {};
-}
-
-
-
-async function handleSignIn(email, password) {
-    const response = await signInUser(email, password);
-    checkAuth(response);
-}
-
-async function handleSignUp(email, password) {
-    const response = await signUpUser(email, password);
-    checkAuth(response);
-}
